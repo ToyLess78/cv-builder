d56ca56b30eec00d97bc7fad6ac5ba219823d339
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-17 22:57:01 +0300

    + update Main component

--- src/libs/components/Main/Main.tsx
+++ src/libs/components/Main/Main.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import styles from './Main.module.css';
-import {
-    BreezeTitle,
-    Certificates,
-    EditeWrapper,
-    MonthYearPickerSingle,
-    MonthYearPickerWithRange,
-    UnderlineInput
-} from '~/components';
-import { useSelector } from 'react-redux';
-import { RootState } from '~/store/store';
-import { selectCertificates } from '~/slices/certificatesSlice';
+import { MonthYearPickerSingle, MonthYearPickerWithRange } from '~/components';
+
 
 
 interface IMainProps {
@@ -18,35 +9,11 @@ interface IMainProps {
 }
 
 export const Main: React.FC<IMainProps> = ({ isOrder }) => {
-    const certificates = useSelector((state: RootState) => selectCertificates(state));
 
     return (
         <section className={styles.main} style={isOrder ? { order: 2 } : {}}>
             <MonthYearPickerWithRange/>
             <MonthYearPickerSingle />
-            <EditeWrapper
-                preview={
-                    < Certificates data={certificates}>
-                        <BreezeTitle text={certificates.title} />
-                    </Certificates>
-                }
-                edite={
-                <>
-                    <UnderlineInput
-                        label="title"
-                        onChange={(e) => console.log(e.currentTarget.value)}
-                    />
-                    <UnderlineInput
-                        label="issue"
-                        onChange={(e) => console.log(e.currentTarget.value)}
-                    />
-                    <UnderlineInput
-                        label="link"
-                        onChange={(e) => console.log(e.currentTarget.value)}
-                    />
-                </>
-                }
-            />
 
         </section>
 
