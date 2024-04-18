d6e02191fd8ca4774c32a8f300081f806b30fb80
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-18 21:20:50 +0300

    + add levels array

---
+++ public/levels.ts
@@ -0,0 +1,11 @@
+const levels = [
+    { name: 'Beginner', code: 'A1' },
+    { name: 'Elementary', code: 'A2' },
+    { name: 'Intermediate', code: 'B1' },
+    { name: 'Upper-Intermediate', code: 'B2' },
+    { name: 'Advanced', code: 'C1' },
+    { name: 'Proficiency', code: 'C2' },
+    { name: 'Native', code: 'N' }
+];
+
+export default levels;
