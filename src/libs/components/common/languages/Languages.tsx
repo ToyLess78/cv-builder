03f911abe94d912ceb9632a25a2a40fe60c92098
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-20 22:22:28 +0300

    + update Languages component

--- src/libs/components/common/languages/Languages.tsx
+++ src/libs/components/common/languages/Languages.tsx
@@ -1,21 +1,21 @@
 import React, { ReactNode } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '~/store/store';
-import { selectLanguages, setIsLanguages } from '~/slices/languagesSlice';
+import { ILanguage, selectLanguages, setIsLanguages } from '~/slices/languagesSlice';
 import { AsideItem } from '../../Aside/AsideItem';
-import { FaRegEdit } from 'react-icons/fa';
-import { BiHide } from 'react-icons/bi';
-import nextId from 'react-id-generator';
-import { MdOutlineVisibility } from 'react-icons/md';
+import styles from './Languages.module.scss';
+import { EditButton, HideButton, RemoveButton, ShowButton } from '~/components/common/Buttons/Buttons';
+import { setIsEdit } from '~/slices/editSlice';
 
 interface ILanguagesProps {
     children: ReactNode;
+    data?: ILanguage[] | null;
 }
 
-export const Languages: React.FC<ILanguagesProps> = ({ children }) => {
+export const Languages: React.FC<ILanguagesProps> = ({children, data = null}) => {
 
     const languages = useSelector((state: RootState) => selectLanguages(state));
-    const { isLanguages } = languages;
+    const {isLanguages} = languages;
     const dispatch = useDispatch();
     const handleSetIsLanguages = () => {
         dispatch(setIsLanguages(!isLanguages));
@@ -23,41 +23,45 @@ export const Languages: React.FC<ILanguagesProps> = ({ children }) => {
 
     return (
         <>
-            {!isLanguages &&
-                <div className='show'>
-                    <MdOutlineVisibility
-                        size='1.2rem'
-                        className='hide'
-                        data-tooltip-id='tooltip'
-                        data-tooltip-content='Show Languages'
-                        data-tooltip-offset={0}
-                        onClick={handleSetIsLanguages}
-                    />
-                </div>}
-            {languages && isLanguages &&
+            { !isLanguages && !data &&
+                <ShowButton
+                    onClick={ handleSetIsLanguages }
+                    title="Languages"
+                />
+            }
+            {!data && languages && isLanguages &&
                 <AsideItem>
-                    {isLanguages &&
+                    { isLanguages &&
                         <>
-                            <FaRegEdit
-                                className='edit'
-                                data-tooltip-id='tooltip'
-                                data-tooltip-content='Edit Languages'
-                                data-tooltip-offset={0}
+                            <EditButton
+                                title="languages"
+                                onClick={ () => dispatch(setIsEdit('languages')) }
                             />
-                            <BiHide
-                                size='1.2rem'
-                                className='hide'
-                                data-tooltip-id='tooltip'
-                                data-tooltip-content='Hide Languages'
-                                data-tooltip-offset={20}
-                                onClick={handleSetIsLanguages}
+                            <HideButton
+                                onClick={ handleSetIsLanguages }
+                                title="languages"
+
                             />
                         </>}
                     {children}
-                    <ul className='languages'>
+                    <ul className={ styles.languages }>
                         {languages.data?.map(l => {
-                            return <li key={nextId()}><span>{l[0]}</span><p>{l[1]}</p></li>
-                        })}
+                            return <li key={ l.id }><span>{ l.language.name }</span>
+                                <p>{ l.level.name }</p>
+                            </li>;
+                        }) }
+                    </ul>
+                </AsideItem>
+            }
+            {data && isLanguages &&
+                <AsideItem>
+                    {children}
+                    <ul className={ styles.languages }>
+                        {data?.map(l => {
+                            return <li key={ l.id }><span>{ l.language.name }</span>
+                                <p>{ l.level.name }</p><RemoveButton/>
+                            </li>;
+                        }) }
                     </ul>
                 </AsideItem>
             }
