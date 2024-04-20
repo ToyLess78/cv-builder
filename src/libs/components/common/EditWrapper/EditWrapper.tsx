bb6327b526a0965b7083bae2be1c2c93886042a1
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-20 22:18:27 +0300

    + complement EditWrapper.tsx

--- src/libs/components/common/EditWrapper/EditWrapper.tsx
+++ src/libs/components/common/EditWrapper/EditWrapper.tsx
@@ -6,12 +6,15 @@ interface IEditeWrapperProps {
     preview: ReactNode;
     edit: ReactNode;
     onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+    isGrow?: boolean;
 }
 
-export const EditWrapper: React.FC<IEditeWrapperProps> = ({preview, edit, onSubmit}) => {
+export const EditWrapper: React.FC<IEditeWrapperProps> = ({preview, edit, onSubmit, isGrow = false}) => {
     return (
         <div className={ styles.wrapper }>
+            <div style={{flexGrow: isGrow ? '1' : '0'}}>
             { preview }
+            </div>
             <form className={ styles.form } onSubmit={ onSubmit }>
                 { edit }
                 <MainButton type='submit'>Save Changes</MainButton>
