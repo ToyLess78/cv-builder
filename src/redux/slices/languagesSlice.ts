d583ac876a955da4d46aa08aa3b5a46785a62857
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-20 22:16:40 +0300

    + improve languagesSlice.ts

--- src/redux/slices/languagesSlice.ts
+++ src/redux/slices/languagesSlice.ts
@@ -2,28 +2,69 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '~/store/store';
 import { loadFromLocalStorage, removeFromLocalStorage, saveToLocalStorage } from '~/utils/utils';
 
-interface LanguagesState {
+interface ILanguagesState {
         isLanguages: boolean,
-        data: string[][]
+        data: ILanguage[]
+}
+
+export interface IItems {
+    name: string;
+    code: string;
+}
+
+export interface ILanguage {
+    id: string,
+    language: IItems,
+    level: IItems
 }
 
 const defaultLanguages = {
     isLanguages: true,
     data:
         [
-            ['English', 'Intermediate'],
-            ['Ukrainian', 'Native'],
-            ['Russian', 'Advanced']
+            {
+                id: 'id261',
+                language: {
+                    code: 'en',
+                    name: 'English'
+                },
+                level: {
+                    name: 'Intermediate',
+                    code: 'B1'
+                }
+            },
+            {
+                id: 'id291',
+                language: {
+                    code: 'uk',
+                    name: 'Ukrainian'
+                },
+                level: {
+                    name: 'Native',
+                    code: 'N'
+                }
+            },
+            {
+                id: 'id276',
+                language: {
+                    code: 'ru',
+                    name: 'Russian'
+                },
+                level: {
+                    name: 'Advanced',
+                    code: 'C1'
+                }
+            },
         ]
 }
 
-const initialState: LanguagesState = loadFromLocalStorage('languages') || defaultLanguages;
+const initialState: ILanguagesState = loadFromLocalStorage('languages') || defaultLanguages;
 
 const languagesSlice = createSlice({
     name: 'languages',
     initialState,
     reducers: {
-        setLanguages(state, action: PayloadAction<Partial<LanguagesState>>) {
+        setLanguages(state, action: PayloadAction<Partial<ILanguagesState>>) {
             const newState = {
                 ...state,
                 ...action.payload
