9a82508e33d3e9c06e30f3c86529330f3a1d7134
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-22 10:55:49 +0300

    + update Info component

--- src/redux/slices/infoSlice.ts
+++ src/redux/slices/infoSlice.ts
@@ -7,15 +7,15 @@ interface InfoState {
     lastname: string;
     position: string;
     title: string;
-    introduction: string;
+    summary: string;
 }
 
 const initialState: InfoState = loadFromLocalStorage('info') || {
     firstname: 'Your',
     lastname: 'Name',
     position: 'Web Developer',
-    title: 'about',
-    introduction: '<p>Experienced web developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.</p>'
+    title: 'summary',
+    summary: '<p>Experienced web developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.</p>'
 };
 
 const infoSlice = createSlice({
