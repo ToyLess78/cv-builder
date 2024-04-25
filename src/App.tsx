6dcefbc7b74caa9eb4bf59d19ff9b81d89a0045c
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-25 20:30:33 +0300

    + replace dialog logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import { RootState } from '~/store/store';
 import { selectThemeColor } from '~/slices/themeSlice';
 import React, { lazy, Suspense, useEffect, useState } from 'react';
 import { setAlphaToRGBA } from '~/utils/color.utils';
-import { MenuOverlay } from '~/components/common/Overlay/Overlay';
+import { MenuOverlay, Overlay } from '~/components/common/Overlay/Overlay';
 import { Loading, MoreButton, SaveButton } from '~/components';
 import { resetId } from 'react-id-generator';
+import { selectIsEdit } from '~/slices/editSlice';
 
 const Menu = lazy(() => import('~/components/common/Carousel/Carousel'));
+const EditCertificates = lazy(() => import('~/components/certificates/EditCertificates'));
+const EditAbout = lazy(() => import('~/components/header/EditAbout'));
+const EditInfo = lazy(() => import('~/components/header/EditInfo'));
+const EditSkills = lazy(() => import('~/components/skills/EditSkills'));
+const EditLanguage = lazy(() => import('~/components/languages/EditLanguage'));
+const EditContacts = lazy(() => import('~/components/contacts/EditContacts'));
+
 
 const App: React.FC = () => {
     resetId();
@@ -25,12 +33,25 @@ const App: React.FC = () => {
 
     // const templates = ['success', 'advance', 'headway', 'breeze', 'strong', 'precise', 'serene', 'modern', 'fortune', 'recency', 'verdure', 'master', 'primary', 'prime', 'grand', 'alpha', 'galaxy', 'goodly', 'gallant', 'winner', 'elegant', 'future']
     // style={{'--primary': themeColor, '--primary-opacity': setAlphaToRGBA(themeColor as string, 0.1)} as React.CSSProperties}
+    const isEdit = useSelector((state: RootState) => selectIsEdit(state));
+
     return (
         <
         >
             {/*<Buttons hoverColor='#1976D2'>Buttons 1</Buttons>*/ }
             {/*<Buttons hoverColor='#1976D2'>Buttons 2</Buttons>*/ }
             {/*<Example/>*/ }
+            <Overlay>
+                <Suspense fallback={ <Loading/> }>
+                    { isEdit === 'additional' && <EditSkills isAdditional/> }
+                    { isEdit === 'info' && <EditInfo/> }
+                    { isEdit === 'about' && <EditAbout/> }
+                    { isEdit === 'skills' && <EditSkills/> }
+                    { isEdit === 'certificates' && <EditCertificates/> }
+                    { isEdit === 'languages' && <EditLanguage/> }
+                    { isEdit === 'contacts' && <EditContacts/> }
+                </Suspense>
+            </Overlay>
             <MenuOverlay { ...{isOpen, setIsOpen} }>
                 <Suspense fallback={ <Loading/> }>
                     { isOpen && <Menu{ ...{isOpen, setIsOpen} }/> }
--- src/temeplates/Breeze.tsx
+++ src/temeplates/Breeze.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '~/store/store';
 import { selectSkills } from '~/slices/skillsSlice';
@@ -14,20 +14,10 @@ import {
     Contacts,
     Header,
     Languages,
-    Loading,
     Main,
-    Overlay,
     Skills
 } from '~/components/components';
-import { selectIsEdit } from '~/slices/editSlice';
 import { breezePalette } from '~/public/palettes';
-
-const EditCertificates = lazy(() => import('~/components/certificates/EditCertificates'));
-const EditAbout = lazy(() => import('~/components/header/EditAbout'));
-const EditInfo = lazy(() => import('~/components/header/EditInfo'));
-const EditSkills = lazy(() => import('~/components/skills/EditSkills'));
-const EditLanguage = lazy(() => import('~/components/languages/EditLanguage'));
-const EditContacts = lazy(() => import('~/components/contacts/EditContacts'));
 
 
 export const Breeze: React.FC = () => {
@@ -39,24 +29,12 @@ export const Breeze: React.FC = () => {
     const [color, setColor] = useState(loadFromLocalStorage(theme) || breezePalette[0]);
 
 
-    const isEdit = useSelector((state: RootState) => selectIsEdit(state));
 
     return (
         <>
-            <Overlay>
-                <Suspense fallback={ <Loading/> }>
-                    { isEdit === 'additional' && <EditSkills isAdditional/> }
-                    { isEdit === 'info' && <EditInfo/> }
-                    { isEdit === 'about' && <EditAbout/> }
-                    { isEdit === 'skills' && <EditSkills/> }
-                    { isEdit === 'certificates' && <EditCertificates/> }
-                    { isEdit === 'languages' && <EditLanguage/> }
-                    { isEdit === 'contacts' && <EditContacts/> }
-                </Suspense>
-            </Overlay>
+
             <ColourPicker { ...{theme, palette: breezePalette, color, setColor} } />
             <Header/>
-
 
             <Body>
                 <Main isOrder={false}/>
@@ -81,6 +59,7 @@ export const Breeze: React.FC = () => {
                     <Contacts>
                         <BreezeTitle text='contacts'/>
                     </Contacts>
+
                 </Aside>
             </Body>
         </>
