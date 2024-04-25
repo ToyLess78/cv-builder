a9cb05c299d9b844f11166eb51161b1a4a737744
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-04-25 10:46:41 +0300

    + finished logic & styles EditContacts module

--- src/libs/components/contacts/EditContacts.tsx
+++ src/libs/components/contacts/EditContacts.tsx
@@ -1,45 +1,54 @@
 import React, { useState } from 'react';
 import { BreezeTitle, CheckBox, Contacts, EditWrapper, UnderlineInput } from '~/components';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '~/store/store';
-import { selectContacts } from '~/slices/contactSlice';
+import { selectContacts, setContacts } from '~/slices/contactSlice';
 import styles from './Contacts.module.scss';
 import { TabPanel, TabView } from 'primereact/tabview';
 import { SpeedDial } from 'primereact/speeddial';
 import { MenuItem } from 'primereact/menuitem';
 import { IconsMap } from '~/components/contacts/IconsMap';
+import { setIsEdit } from '~/slices/editSlice';
 
 const EditContacts: React.FC = () => {
     const contactState = useSelector((state: RootState) => selectContacts(state));
-    const [contacts, setContacts] = useState(contactState);
+    const [editContacts, setEditContacts] = useState(contactState);
 
     const setLocation = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setContacts({...contacts, location: e.currentTarget.value});
+        setEditContacts({...editContacts, location: e.currentTarget.value});
     };
     const setIsSocials = () => {
-        setContacts({...contacts, isSocials: !contacts.isSocials});
+        setEditContacts({...editContacts, isSocials: !editContacts.isSocials});
     };
 
     const setEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setContacts({...contacts, email: e.currentTarget.value});
+        setEditContacts({...editContacts, email: e.currentTarget.value});
     };
 
     const setPhone = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setContacts({...contacts, phone: e.currentTarget.value});
+        setEditContacts({...editContacts, phone: e.currentTarget.value});
     };
 
     const editLink = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
-        setContacts({
-            ...contacts,
-            data: contacts.data.map(x => (x.id === id ? {...x, link: e.currentTarget.value} : x))
+        setEditContacts({
+            ...editContacts,
+            data: editContacts.data.map(x => (x.id === id ? {...x, link: e.currentTarget.value} : x))
         });
     };
 
     const setIsShow = (id: string) => {
-        setContacts({...contacts, data: contacts.data.map(x => (x.id === id ? {...x, isShow: !x.isShow} : x))});
+        setEditContacts({...editContacts, data: editContacts.data.map(x => (x.id === id ? {...x, isShow: !x.isShow} : x))});
     };
 
-    const menuItems: MenuItem[] = contacts.data
+    const dispatch = useDispatch();
+
+    const handlerOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(setContacts(editContacts));
+        dispatch(setIsEdit(''));
+    };
+
+    const menuItems: MenuItem[] = editContacts.data
         .filter(s => s.isShow === false)
         .map(s => (
             {
@@ -52,7 +61,7 @@ const EditContacts: React.FC = () => {
     return (
         <EditWrapper
             preview={
-                <Contacts data={ contacts }>
+                <Contacts data={ editContacts }>
                     <BreezeTitle text="contacts"/>
                 </Contacts>
             }
@@ -61,32 +70,32 @@ const EditContacts: React.FC = () => {
                     <div className={ styles.column }>
                         <UnderlineInput
                             label="location"
-                            value={ contacts.location }
+                            value={ editContacts.location }
                             onChange={ setLocation }
                         />
 
                         <UnderlineInput
                             type="email"
                             label="email"
-                            value={ contacts.email }
+                            value={ editContacts.email }
                             onChange={ setEmail }
                         />
 
                         <UnderlineInput
                             label="phone"
                             type="tel"
-                            value={ contacts.phone }
+                            value={ editContacts.phone }
                             onChange={ setPhone }
                         />
 
                         <CheckBox
-                            checked={ contacts.isSocials }
+                            checked={ editContacts.isSocials }
                             onChange={ setIsSocials }
                             title="Show socials"
                         />
                     </div>
 
-                    { contacts.isSocials &&
+                    { editContacts.isSocials &&
                         <>
                             <SpeedDial
                                 model={ menuItems } direction="left"
@@ -95,11 +104,12 @@ const EditContacts: React.FC = () => {
                                 data-tooltip-content="Add social"/>
 
                             <TabView className={ styles.tab }>
-                                { contacts.data?.map((s) => {
+                                { editContacts.data?.map((s) => {
                                     return (
                                         s.isShow &&
                                         <TabPanel
-                                            className={ styles.item } key={ s.id }
+                                            className={ styles.item }
+                                            key={ s.id }
                                             header={ IconsMap.get(s?.id) }>
                                             <div style={ {padding: '2.5rem 0'} }>
                                                 <UnderlineInput
@@ -120,8 +130,8 @@ const EditContacts: React.FC = () => {
                             </TabView>
                         </> }
                 </div>
-
             }
+            onSubmit={ handlerOnSubmit }
         />
     )
 };
