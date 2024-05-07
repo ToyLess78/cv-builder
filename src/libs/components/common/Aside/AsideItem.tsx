d565e0fdf3c8909ae63f1ffb03c1a023f824abcd
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-07 15:53:01 +0300

    + update Aside component

--- src/libs/components/common/Aside/AsideItem.tsx
+++ src/libs/components/common/Aside/AsideItem.tsx
@@ -1,14 +1,21 @@
 import React, { ReactNode } from 'react';
 import styles from './Aside.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '~/store/store';
+import { selectTheme } from '~/slices/theme.slice';
 
 interface IAsideItemProps {
-    children?: ReactNode | undefined
+    children?: ReactNode | undefined;
 }
 
-export const AsideItem: React.FC<IAsideItemProps> = ({ children } ) => {
+export const AsideItem: React.FC<IAsideItemProps> = ({children}) => {
+    const themeState = useSelector((state: RootState) => selectTheme(state));
+    const template = themeState.template;
+
     return (
-        <div className={styles.item}>
-            {children}
+        <div className={ styles.item } style={ template === 'breeze' ? {flexDirection: 'row', gap: '2rem 0'} :{flexDirection: 'column',
+            gap: '.5rem 0', padding: '0 1rem'} }>
+            { children }
         </div>
-    )
-}
+    );
+};
