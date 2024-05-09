866e8d7264ceecfb42e89a9b8392eaf6e078e117
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-10 00:11:57 +0300

    + add CollapsedWrapper component

---
+++ src/libs/components/common/CollapsedWrapper/CollapsedWrapper.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+import styles from './CollapsedWrapper.module.scss';
+
+interface ICollapsedWrapperProps {
+    isShow: boolean;
+    buttons?: ReactNode;
+    content: ReactNode;
+}
+
+export const CollapsedWrapper: React.FC<ICollapsedWrapperProps> = ({isShow, buttons, content}) => {
+    return (
+        <div className={ `${ styles.collapsed } ${ isShow ? styles.open : '' }` }>
+            {isShow && buttons ? buttons : ''}
+            <div className={ styles.content }>
+                { content }
+            </div>
+
+        </div>
+    );
+};
