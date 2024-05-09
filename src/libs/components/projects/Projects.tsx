e484bf45d8021671ecf5db9629ff0d6c62144783
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-10 00:13:44 +0300

    + update Projects component

--- src/libs/components/projects/Projects.tsx
+++ src/libs/components/projects/Projects.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '~/store/store';
 import { IProject, removeProject, selectProjects, setEditedProjectId, setIsProjects } from '~/slices/projects.slice';
 import styles from './Projects.module.scss';
-import { AddItemButton, EditButton, HideButton, RemoveButton, ShowAsideButton } from '~/components';
+import { AddItemButton, CollapsedWrapper, EditButton, HideButton, RemoveButton, ShowAsideButton } from '~/components';
 import { setIsEdit } from '~/slices/edit.slice';
 import nextId from 'react-id-generator';
 import { FiExternalLink } from 'react-icons/fi';
@@ -38,53 +38,65 @@ export const Projects: FC<IProjectsProps> = ({children, projectsItem = null}) =>
 
     return (
         <>
-            { isProjects && !projectsItem &&
-                <section className={`${styles.projects} ${styles[template]}`}>
-                    { children }
-                    <div className={ styles.wrapper }>
-                        { data.map(pro => (
-                            <div key={ pro.id } className={ styles.title }>
-                                <EditButton
-                                    style={ {left: template === TemplateConstants.Breeze ? '-3.7rem' : '-1.9rem'} }
-                                    title={ pro.projectName }
-                                    onClick={ () => handlerSetEdit(pro.id) }
-                                />
-                                { data.length > 1 && <RemoveButton
-                                    style={ {left: template === TemplateConstants.Breeze ? '-3.8rem' : '-2.1rem', top: '1.3rem'} }
-                                    removeOffset={ 20 }
-                                    onRemove={ () => dispatch(removeProject(pro.id)) }
-                                /> }
-                                <div className={ styles.name }>
-                                    <strong className={ styles.dots }>{ pro.projectName }</strong>{ pro.link ?
-                                    <a className={ styles.link } href={ pro.link }><FiExternalLink/></a> : '' }
-                                </div>
+            { !projectsItem &&
+                <CollapsedWrapper
+                    isShow={isProjects}
 
-                                { pro.technologies.length ? <strong><small
-                                >{ `[ ${ pro.technologies.join(', ') } ]` }</small></strong> : '' }
-                                <em style={ {
-                                    textTransform: 'capitalize'
-                                } }>{ pro.type }</em>
-                                <br/>
-                                { pro.duration ? <span className={ styles.duration }>{ pro.duration }</span> : '' }
-                                <div className={ styles.description }
-                                     dangerouslySetInnerHTML={ {__html: pro.description} }></div>
+                    buttons={
+                    <>
+                        <HideButton
+                            title={ title }
+                            style={ {bottom: '3rem'} }
+                            offset={ 0 }
+                            onClick={ () => dispatch(setIsProjects(false)) }
+                        />
+                        <AddItemButton
+                            style={ {bottom: '2rem'} }
+                            title="project"
+                            onClick={ handlerAddProject }
+                        />
+                    </>
+                    }
 
+                    content={
+                        <section className={`${styles.projects} ${styles[template]}`}>
+                            { children }
+                            <div className={ styles.wrapper }>
+                                { data.map(pro => (
+                                    <div key={ pro.id } className={ styles.title }>
+                                        <EditButton
+                                            style={ {left: template === TemplateConstants.Breeze ? '-3.7rem' : '-1.9rem'} }
+                                            title={ pro.projectName }
+                                            onClick={ () => handlerSetEdit(pro.id) }
+                                        />
+                                        { data.length > 1 && <RemoveButton
+                                            style={ {left: template === TemplateConstants.Breeze ? '-3.8rem' : '-2.1rem', top: '1.3rem'} }
+                                            removeOffset={ 20 }
+                                            onRemove={ () => dispatch(removeProject(pro.id)) }
+                                        /> }
+                                        <div className={ styles.name }>
+                                            <strong className={ styles.dots }>{ pro.projectName }</strong>{ pro.link ?
+                                            <a className={ styles.link } href={ pro.link }><FiExternalLink/></a> : '' }
+                                        </div>
+
+                                        { pro.technologies.length ? <strong><small
+                                        >{ `[ ${ pro.technologies.join(', ') } ]` }</small></strong> : '' }
+                                        <em style={ {
+                                            textTransform: 'capitalize'
+                                        } }>{ pro.type }</em>
+                                        <br/>
+                                        { pro.duration ? <span className={ styles.duration }>{ pro.duration }</span> : '' }
+                                        <div className={ styles.description }
+                                             dangerouslySetInnerHTML={ {__html: pro.description} }></div>
+
+                                    </div>
+                                )) }
                             </div>
-                        )) }
-                    </div>
-                    <HideButton
-                        title={ title }
-                        style={ {bottom: '3rem'} }
-                        offset={ 0 }
-                        onClick={ () => dispatch(setIsProjects(false)) }
-                    />
-                    <AddItemButton
-                        style={ {bottom: '2rem'} }
-                        title="project"
-                        onClick={ handlerAddProject }
-                    />
-                </section>
+                        </section>
+                    }
+                />
             }
+
             {!isProjects && !projectsItem &&
                 <div className={`${styles.show} ${styles[template]}`}>
                     <ShowAsideButton
