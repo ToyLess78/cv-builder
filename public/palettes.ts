98bf0c1e9544f099a8eb567a3510a239e8fa7787
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-20 11:48:26 +0300

    + update palettes.ts

--- public/palettes.ts
+++ public/palettes.ts
@@ -29,14 +29,27 @@ export const breezePalette = [
 const accentPalette = [
     'rgba(234, 187, 70, 1)',
     'rgba(164, 145, 49, 1)',
-    'rgba(202, 219, 9, 1)',
-    'rgba(132, 164, 49, 1)',
+    'rgba(132, 165, 157, 1)',
     'rgba(96, 115, 34, 1)',
-    'rgba(144, 49, 164, 1)',
-    'rgba(101, 34, 115, 1)',
-    'rgba(153, 10, 67, 1)',
+    'rgba(157, 129, 137, 1)',
+    'rgba(142, 125, 190, 1)',
+    'rgba(61, 52, 139, 1)',
+    'rgba(114, 0, 38, 1)',
     'rgba(49, 143, 164, 1)',
-    'rgba(91, 49, 164, 1)'
+    'rgba(47, 102, 144, 1)'
+];
+
+const advancePalette = [
+    'rgba(32, 42, 81, 1)',
+    'rgba(22, 38, 57, 1)',
+    'rgba(35, 22, 57, 1)',
+    'rgba(42, 37, 81, 1)',
+    'rgba(50, 32, 81, 1)',
+    'rgba(32, 81, 66, 1)',
+    'rgba(22, 57, 46, 1)',
+    'rgba(81, 78, 32, 1)',
+    'rgba(57, 41, 22, 1)',
+    'rgba(57, 22, 32, 1)'
 ];
 
 const headwayPalette = [
@@ -51,11 +64,42 @@ const headwayPalette = [
     'rgba(128, 128, 0, 1)',
     'rgba(57, 53, 22, 1)'
 ];
+const successPalette = [
+    'rgba(31, 76, 81, 1)',
+    'rgba(22, 54, 57, 1)',
+    'rgba(32, 43, 81, 1)',
+    'rgba(31, 81, 34, 1)',
+    'rgba(22, 57, 24, 1)',
+    'rgba(81, 31, 66, 1)',
+    'rgba(57, 22, 46, 1)',
+    'rgba(81, 72, 31, 1)',
+    'rgba(39, 37, 81, 1)',
+    'rgba(27, 26, 57, 1)'
+];
+
+const modernPalette = [
+    'rgba(67, 159, 195, 1)',
+    'rgba(47, 123, 136, 1)',
+    'rgba(0, 119, 182, 1)',
+    'rgba(142, 202, 230, 1)',
+    'rgba(2, 48, 71, 1)',
+    'rgba(255, 183, 3, 1)',
+    'rgba(27, 38, 59, 1)',
+    'rgba(65, 90, 119, 1)',
+    'rgba(119, 141, 169, 1)',
+    'rgba(214, 204, 194, 1)',
+    'rgba(213, 189, 175, 1)',
+    'rgba(94, 84, 142, 1)',
+    'rgba(159, 134, 192, 1)'
+];
 
 const palettesMap = new Map();
 palettesMap.set(TemplateConstants.Strong, strongPalette);
 palettesMap.set(TemplateConstants.Breeze, breezePalette);
 palettesMap.set(TemplateConstants.Accent, accentPalette);
+palettesMap.set(TemplateConstants.Advance, advancePalette);
 palettesMap.set(TemplateConstants.Headway, headwayPalette);
+palettesMap.set(TemplateConstants.Success, successPalette);
+palettesMap.set(TemplateConstants.Modern, modernPalette);
 
 export default palettesMap;
