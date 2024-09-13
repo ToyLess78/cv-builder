122ddc95ec6b80483ac4b1a715b1691c28402acd
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-09-13 21:15:28 +0300

    + formatted public

--- public/data.ts
+++ public/data.ts
@@ -1,136 +1,149 @@
 export const data = {
-    info: {
-        name: 'Your Name',
-        position: 'Trainee JavaScript Developer',
-        title: 'about',
-        introduction: 'A Software Developer with entry-level experience specializing in user interface design, application development, data security, and continuous improvement. Adept at identifying opportunities to enhance the user experience and develop new features to improve app functionality. As a burgeoning developer embarking on the initial phases of my journey, I am fervently driven to bolster my skillsTechnologies and expertise. I recognize *COMPANY* as a pivotal platform that aligns seamlessly with my aspirations in the field of development. My unyielding conviction lies in the belief that the realm of coding presents an array of untapped opportunities for me to delve into and adeptly conquer.'
-    },
-    aside: {
-        skills: [
-            'JavaScript (ES6+)',
-            'Typescript',
-            'React.js',
-            'NodeJS',
-            'ExpressJS',
-            'Next.js',
-            'HTML5',
-            'CSS3',
-            'Git'
-        ],
+	info: {
+		name: "Your Name",
+		position: "Trainee JavaScript Developer",
+		title: "about",
+		introduction:
+			"A Software Developer with entry-level experience specializing in user interface design, application development, data security, and continuous improvement. Adept at identifying opportunities to enhance the user experience and develop new features to improve app functionality. As a burgeoning developer embarking on the initial phases of my journey, I am fervently driven to bolster my skillsTechnologies and expertise. I recognize *COMPANY* as a pivotal platform that aligns seamlessly with my aspirations in the field of development. My unyielding conviction lies in the belief that the realm of coding presents an array of untapped opportunities for me to delve into and adeptly conquer.",
+	},
+	aside: {
+		skills: [
+			"JavaScript (ES6+)",
+			"Typescript",
+			"React.js",
+			"NodeJS",
+			"ExpressJS",
+			"Next.js",
+			"HTML5",
+			"CSS3",
+			"Git",
+		],
 
-        additional: [
-            'Redux Toolkit',
-            'RTK Query',
-            'REST API',
-            'WebSocket',
-            'CI/CD',
-            'CSCC',
-            'BEM'
-        ],
-
-    },
-    certifications: [
-        {
-            id: '6486462gfd',
-            title: 'IT security: protection against digital cyber fraud',
-            issue: 'Google',
-            link: 'https://www.coursera.org/account/accomplishments/certificate/NUAE2JM7NCNZ',
-            at: 'Nov 2023'
-        },        {
-            id: '64hhhtg2gfd',
-            title: 'JavaScript Algorithms and Data Structures',
-            issue: 'freeCodeCamp',
-            link: 'https://www.freecodecamp.org/certification/fccf30fb616-0d1f-4b9f-acad-6b570afb74c2a/javascript-algorithms-and-data-structures',
-            at: 'Nov 2023'
-        },
-        {
-            id: '64gfg62gfd',
-            title: 'Layout and Creation of Websites from Scratch',
-            issue: 'Udemy',
-            link: 'https://www.udemy.com/certificate/UC-ef47402f-9686-4f79-a2c3-84528ccbc79a/',
-            at: 'Nov 2023'
-        },
-        {
-            id: '648sgvg2gfd',
-            title: 'React, React Router & Redux Toolkit',
-            issue: 'Udemy',
-            link: 'https://www.udemy.com/certificate/UC-6760c53d-b563-411c-af04-f760baad6e26/',
-            at: 'Mar 2024'
-        }
-    ],
-    contacts:
-        {
-            location: 'Ukraine',
-            email: 'hello@example.com',
-            phone: '001122334455',
-            github: 'https://github.com/ToyLess78',
-            linkedin: 'https://www.linkedin.com',
-            telegram: 'https://t.me/JATly'
-        },
-    languages: [
-        ['English', 'Intermediate'],
-        ['Ukrainian', 'Native'],
-        ['Russian', 'Advanced']
-    ],
-    templates: ['breeze', 'strong', 'galaxy'],
-    education: {
-        title: 'education',
-        editedId: '',
-        isEducations: true,
-        data: [
-            {
-                id: '31T22000000Z',
-                duration: '2011 - 2015',
-                isYear: true,
-                isPresent: false,
-                school: 'Pepperdine University',
-                degree: 'Master of Computer Science',
-                location: 'Malibu',
-                description: ''
-            },
-            {
-                id: '30T210000sddZ',
-                duration: '2007 - 2010',
-                isYear: true,
-                isPresent: false,
-                school: 'University of California at Los Angeles',
-                degree: 'Bachelor of Computer Science',
-                location: 'Los Angeles',
-                description: ''
-            }
-        ]
-    },
-    projects: {
-        title: 'projects',
-        editedId: '',
-        isProjects: true,
-        data: [
-            {
-                id: 'PodcastPlatform',
-                projectName: 'Podcast Platform',
-                link: '',
-                duration: '2011 - 2015',
-                isDuration: true,
-                isYear: true,
-                isPresent: false,
-                technologies: ['TypeScript', 'React', 'Node.js', 'PostgreSQL', 'Objection', 'WebRTC'],
-                type: 'personal project',
-                description: 'A platform for recording, storing, and supporting podcasts. The solution allows you to record a podcast in real time (people can go in and comment on the podcast while recording). Users can create favorite podcast lists, leave comments and likes. A private podcast option is also available.'
-            },
-            {
-                id: 'FundraisingPlatform',
-                projectName: 'Fundraising Platform',
-                link: '',
-                duration: '2011 - 2015',
-                isDuration: true,
-                isYear: true,
-                isPresent: false,
-                technologies: ['TypeScript', 'React', 'Node.js', 'PostgreSQL', 'TypeORM'],
-                type: 'team project',
-                description: 'This platform helps people create their own fundraising projects and attract sponsors. The user can support somebody’s idea and donate towards its realization. There are several features such as recommendations for project authors and sponsors, statistics on top themes, diagrams of hype dynamic on certain themes, information about related projects and their current state, and many more.'
-            }
-        ]
-    }
-
-
-}
+		additional: [
+			"Redux Toolkit",
+			"RTK Query",
+			"REST API",
+			"WebSocket",
+			"CI/CD",
+			"CSCC",
+			"BEM",
+		],
+	},
+	certifications: [
+		{
+			id: "6486462gfd",
+			title: "IT security: protection against digital cyber fraud",
+			issue: "Google",
+			link: "https://www.coursera.org/account/accomplishments/certificate/NUAE2JM7NCNZ",
+			at: "Nov 2023",
+		},
+		{
+			id: "64hhhtg2gfd",
+			title: "JavaScript Algorithms and Data Structures",
+			issue: "freeCodeCamp",
+			link: "https://www.freecodecamp.org/certification/fccf30fb616-0d1f-4b9f-acad-6b570afb74c2a/javascript-algorithms-and-data-structures",
+			at: "Nov 2023",
+		},
+		{
+			id: "64gfg62gfd",
+			title: "Layout and Creation of Websites from Scratch",
+			issue: "Udemy",
+			link: "https://www.udemy.com/certificate/UC-ef47402f-9686-4f79-a2c3-84528ccbc79a/",
+			at: "Nov 2023",
+		},
+		{
+			id: "648sgvg2gfd",
+			title: "React, React Router & Redux Toolkit",
+			issue: "Udemy",
+			link: "https://www.udemy.com/certificate/UC-6760c53d-b563-411c-af04-f760baad6e26/",
+			at: "Mar 2024",
+		},
+	],
+	contacts: {
+		location: "Ukraine",
+		email: "hello@example.com",
+		phone: "001122334455",
+		github: "https://github.com/ToyLess78",
+		linkedin: "https://www.linkedin.com",
+		telegram: "https://t.me/JATly",
+	},
+	languages: [
+		["English", "Intermediate"],
+		["Ukrainian", "Native"],
+		["Russian", "Advanced"],
+	],
+	templates: ["breeze", "strong", "galaxy"],
+	education: {
+		title: "education",
+		editedId: "",
+		isEducations: true,
+		data: [
+			{
+				id: "31T22000000Z",
+				duration: "2011 - 2015",
+				isYear: true,
+				isPresent: false,
+				school: "Pepperdine University",
+				degree: "Master of Computer Science",
+				location: "Malibu",
+				description: "",
+			},
+			{
+				id: "30T210000sddZ",
+				duration: "2007 - 2010",
+				isYear: true,
+				isPresent: false,
+				school: "University of California at Los Angeles",
+				degree: "Bachelor of Computer Science",
+				location: "Los Angeles",
+				description: "",
+			},
+		],
+	},
+	projects: {
+		title: "projects",
+		editedId: "",
+		isProjects: true,
+		data: [
+			{
+				id: "PodcastPlatform",
+				projectName: "Podcast Platform",
+				link: "",
+				duration: "2011 - 2015",
+				isDuration: true,
+				isYear: true,
+				isPresent: false,
+				technologies: [
+					"TypeScript",
+					"React",
+					"Node.js",
+					"PostgreSQL",
+					"Objection",
+					"WebRTC",
+				],
+				type: "personal project",
+				description:
+					"A platform for recording, storing, and supporting podcasts. The solution allows you to record a podcast in real time (people can go in and comment on the podcast while recording). Users can create favorite podcast lists, leave comments and likes. A private podcast option is also available.",
+			},
+			{
+				id: "FundraisingPlatform",
+				projectName: "Fundraising Platform",
+				link: "",
+				duration: "2011 - 2015",
+				isDuration: true,
+				isYear: true,
+				isPresent: false,
+				technologies: [
+					"TypeScript",
+					"React",
+					"Node.js",
+					"PostgreSQL",
+					"TypeORM",
+				],
+				type: "team project",
+				description:
+					"This platform helps people create their own fundraising projects and attract sponsors. The user can support somebody’s idea and donate towards its realization. There are several features such as recommendations for project authors and sponsors, statistics on top themes, diagrams of hype dynamic on certain themes, information about related projects and their current state, and many more.",
+			},
+		],
+	},
+};
